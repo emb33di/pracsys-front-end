fa6f26cd91cb07e3ce440c400a6da71c5a5d1154
PeruDayani <perudayani@berkeley.edu>
2025-02-22 14:27:55 -0500

    first one

---
+++ src/utils/types.tsx
@@ -0,0 +1,19 @@
+// Typescript is great
+// What is typescript? https://www.youtube.com/watch?v=zQnBQ4tB3ZA&ab_channel=Fireship
+// Why
+// - You can clearly model your data
+// - This data model can be used by the backend api too
+
+type ProcessingTime = {
+    providerName: string,
+    processingTime: number
+}
+
+type Claim = {
+    id: string,
+    patientName: string,
+    testOrderedName: string,
+    testOrderDate: Date
+    authorizationStatu: 'draft' | 'pending' | 'complete'
+}
+
